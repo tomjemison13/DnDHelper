701995d27870b5adb111dbedf5974278c913d068
thomas jemison <tomjemison13@gmail.com>
2022-05-08 18:22:34 -0400

    L

--- Spells/PHB.cs
+++ Spells/PHB.cs
@@ -3654,32 +3654,224 @@ namespace Spells
 
             #region L
 
+            Spell legendLore = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Legend Lore",
+                SpellLv = SpellLv.fifth,
+                School = SchoolOfMagic.divination,
+                CastingTime = "10 " + StringTime.minutes,
+                Range = Range.self,
+                Verbal = true,
+                Somatic = true,
+                Materials = "incense worth at lease 250gp, which the spell consumes, and four ivory strips worth at least 50gp each",
+                SpellCost = 300,
+                Duration = DndTime.instantaneous,
+                Description = "Name or describe a person, place, or object. The spell brings to your mind a brief summary of the significant lore about the thing you named. The lore might consist of current tales, forgotten stories, or even secret lore that has never been widely known. If the thing you named isn’t of legendary importance, you gain no information. The more information you already have about the thing, the more precise and detailed the information you receive is. The information you learn is accurate but might be couched in figurative language. For example, if you have a mysterious magic axe on hand, the spell might yield this information: “Woe to the evildoer whose hand touches the axe, for even the haft slices the hand of the evil ones. Only a true Child of Stone, lover and beloved of Moradin, may awaken the true powers of the axe, and only with the sacred word Rudnogg on the lips.”",
+                Classes = new string[] { }
+            };
+            Spells.Add(legendLore.Name, legendLore);
 
+            Spell leomundsSecretChest = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Leomund's Secret Chest",
+                SpellLv = SpellLv.fourth,
+                School = SchoolOfMagic.conjuration,
+                CastingTime = DndTime.action,
+                Range = Range.touch,
+                Verbal = true,
+                Somatic = true,
+                Materials = "an exquisite chest, 3 feet by 2 feet by 2 feet, constructed from rare materials worth at least 5,000gp, and a Tiny replica made from the same materials worth at least 50gp",
+                SpellCost = 5050,
+                Duration = DndTime.instantaneous,
+                Description = "You hide a chest, and all its contents, on the Ethereal Plane. You must touch the chest and the miniature replica that serves as a material component for the spell. The chest can contain up to 12 cubic feet of nonliving material (3 feet by 2 feet by 2 feet). While the chest remains on the Ethereal Plane, you can use an action and touch the replica to recall the chest. It appears in an unoccupied space on the ground within 5 feet of you. You can send the chest back to the Ethereal Plane by using an action and touching both the chest and the replica. After 60 days, there is a cumulative 5 percent chance per day that the spell's effect ends. This effect ends if you cast this spell again, if the smaller replica chest is destroyed, or if you choose to end the spell as an action. If the spell ends and the larger chest is on the Ethereal Plane, it is irretrievably lost.",
+                Classes = new string[] { }
+            };
+            Spells.Add(leomundsSecretChest.Name, leomundsSecretChest);
 
+            Spell leomundsTinyHut = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Leomunds's Tiny Hut",
+                SpellLv = SpellLv.third,
+                School = SchoolOfMagic.evocation,
+                Ritual = true,
+                CastingTime = StringTime.minute,
+                Range = Range.self + ", 10" + Range.footRadius + " " + Range.hemisphere,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a small crystal bead",
+                Duration = "8 " + StringTime.hours,
+                Description = "A 10-foot-radius immobile dome of force springs into existence around and above you and remains stationary for the duration. The spell ends if you leave its area. Nine creatures of Medium size or smaller can fit inside the dome with you. The spell fails if its area includes a larger creature or more than nine creatures. Creatures and objects within the dome when you cast this spell can move through it freely. All other creatures and objects are barred from passing through it. Spells and other magical effects can’t extend through the dome or be cast through it. The atmosphere inside the space is comfortable and dry, regardless of the weather outside. Until the spell ends, you can command the interior to become dimly lit or dark. The dome is opaque from the outside, of any color you choose, but it is transparent from the inside.",
+                Classes = new string[] { }
+            };
+            Spells.Add(leomundsTinyHut.Name, leomundsTinyHut);
 
+            Spell lesserRestoration = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Lesser Restoration",
+                SpellLv = SpellLv.second,
+                School = SchoolOfMagic.abjuration,
+                CastingTime = DndTime.action,
+                Range = Range.touch,
+                Verbal = true,
+                Somatic = true,
+                Duration = DndTime.instantaneous,
+                Description = "You touch a creature and can end either one disease or one condition afflicting it. The condition can be blinded, deafend, paralyzed, or poisoned",
+                Classes = new string[] { },
+                SingleTarget = true
+            };
+            Spells.Add(lesserRestoration.Name, lesserRestoration);
 
+            Spell levitate = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Levitate",
+                SpellLv = SpellLv.second,
+                School = SchoolOfMagic.transmutation,
+                CastingTime = DndTime.action,
+                Range = "60 " + Range.feet,
+                Verbal = true,
+                Somatic = true,
+                Materials = "either a small leather loop or a piece of golden wire bent into a cup shape with a long shank on one end",
+                Duration = DndTime.concentration + "10 " + StringTime.minutes,
+                Concentration = true,
+                Description = "One creature or object of your choice that you can see within range rises vertically, up to 20 feet, and remains suspended there for the duration. The spell can levitate a target that weighs up to 500 pounds. An unwilling creature that succeeds on a Constitution saving throw is unaffected. The target can move only by pushing or pulling against a fixed object or surface within reach (such as a wall or a ceiling), which allows it to move as if it were climbing. You can change the target’s altitude by up to 20 feet in either direction on your turn. If you are the target, you can move up or down as part of your move. Otherwise, you can use your action to move the target, which must remain within the spell’s range. When the spell ends, the target floats gently to the ground if it is still aloft.",
+                Classes = new string[] { },
+                SingleTarget = true,
+                SavingThrows = new string[] { SavingThrows.constitution }
+            };
+            Spells.Add(levitate.Name, levitate);
 
+            Spell light = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Light",
+                SpellLv = SpellLv.cantrip,
+                School = SchoolOfMagic.evocation,
+                CastingTime = DndTime.action,
+                Range = Range.touch,
+                Verbal = true,
+                Materials = "a firefly or phosphorescent moss",
+                Duration = StringTime.hour,
+                Concentration = true,
+                Description = "You touch one object that is no larger than 10 feet in any dimension. Until the spell ends, the object sheds bright light in a 20-foot radius and dim light for an additional 20 feet. The light can be colored as you like. Completely covering the object with something opaque blocks the light. The spell ends if you cast it again or dismiss it as an action. If you target an object held or worn by a hostile creature, that creature must succeed on a Dexterity saving throw to avoid the spell.",
+                Classes = new string[] { },
+                SingleTarget = true,
+                SavingThrows = new string[] { SavingThrows.dexterity }
+            };
+            Spells.Add(light.Name, light);
 
+            Spell lightningArrow = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Lightning Arrow",
+                SpellLv = SpellLv.third,
+                School = SchoolOfMagic.transmutation,
+                CastingTime = DndTime.bounusAction,
+                Range = Range.self,
+                Verbal = true,
+                Somatic = true,
+                Duration = DndTime.concentration + StringTime.minute,
+                Concentration = true,
+                Description = "The next time you make a ranged weapon attack during the spell’s duration, the weapon’s ammunition, or the weapon itself if it’s a thrown weapon, transforms into a bolt of lightning. Make the attack roll as normal. The target takes 4d8 lightning damage on a hit, or half as much damage on a miss, instead of the weapon’s normal damage. Whether you hit or miss, each creature within 10 feet of the target must make a Dexterity saving throw. Each of these creatures takes 2d8 lightning damage on a failed save, or half as much damage on a successful one. The piece of ammunition or weapon then returns to its normal form. A t Higher Levels. W hen you cast this spell using a spell slot of 4th level or higher, the damage for both effects of the spell increases by 1d8 for each slot level above 3rd.",
+                Classes = new string[] { },
+                DamageTypes = new string[] { DamageType.lightning }
+            };
+            Spells.Add(lightningArrow.Name, lightningArrow);
 
+            Spell lightningBolt = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Lightning Bolt",
+                SpellLv = SpellLv.third,
+                School = SchoolOfMagic.evocation,
+                CastingTime = DndTime.action,
+                Range = Range.self + ", 100" + Range.footLine,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a bitr of fur and a rod of amber, crystal, or glass",
+                Duration = DndTime.instantaneous,
+                Description = "A stroke of lightning forming a line 100 feet long and 5 feet wide blasts out from you in a direction you choose. Each creature in the line must make a Dexterity saving throw. A creature takes 8d6 lightning damage on a failed save, or half as much damage on a successful one. The lightning ignites flammable objects in the area that aren’t being worn or carried. At Higher Levels. When you cast this spell using a spell slot o f 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.",
+                Classes = new string[] { },
+                SavingThrows = new string[] { SavingThrows.dexterity },
+                DamageTypes = new string[] { DamageType.lightning }
+            };
+            Spells.Add(lightningBolt.Name, lightningBolt);
 
+            Spell locateAnimalsOrPlants = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Locate Animals or Plants",
+                SpellLv = SpellLv.second,
+                School = SchoolOfMagic.divination,
+                Ritual = true,
+                CastingTime = DndTime.action,
+                Range = Range.self,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a bit of fur from a bloodhound",
+                Duration = DndTime.instantaneous,
+                Description = "Describe or name a specific kind of beast or plant. Concentrating on the voice of nature in your surroundings, you learn the direction and distance to the closest creature or plant of that kind within 5 miles, if any are present.",
+                Classes = new string[] { }
+            };
+            Spells.Add(locateAnimalsOrPlants.Name, locateAnimalsOrPlants);
 
+            Spell locateCreature = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Locate Creature",
+                SpellLv = SpellLv.fourth,
+                School = SchoolOfMagic.divination,
+                CastingTime = DndTime.action,
+                Range = Range.self,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a bit of fur from a bloodhound",
+                Duration = DndTime.concentration + StringTime.hour,
+                Concentration = true,
+                Description = "Describe or name a creature that is familiar to you. You sense the direction to the creature’s location, as long as that creature is within 1,000 feet of you. If the creature is moving, you know the direction of its movement. The spell can locate a specific creature known to you, or the nearest creature of a specific kind (such as a human or a unicorn), so long as you have seen such a creature up close—within 30 feet—at least once. If the creature you described or named is in a different form, such as being under the effects of a polymorph spell, this spell doesn’t locate the creature. This spell can’t locate a creature if running water at least 10 feet wide blocks a direct path between you and the creature.",
+                Classes = new string[] { }
+            };
+            Spells.Add(locateCreature.Name, locateCreature);
 
+            Spell locateObject = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Locate Object",
+                SpellLv = SpellLv.second,
+                School = SchoolOfMagic.divination,
+                CastingTime = DndTime.action,
+                Range = Range.self,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a forked twig",
+                Duration = DndTime.concentration + "10 " + StringTime.minutes,
+                Concentration = true,
+                Description = "Describe or name an object that is familiar to you. You sense the direction to the object’s location, as long as that object is within 1,000 feet of you. If the object is in motion, you know the direction of its movement. The spell can locate a specific object known to you, as long as you have seen it up close—within 30 feet—at least once. Alternatively, the spell can locate the nearest object of a particular kind, such as a certain kind of apparel, jewelry, furniture, tool, or weapon. This spell can’t locate an object if any thickness of lead, even a thin sheet, blocks a direct path between you and the object.",
+                Classes = new string[] { }
+            };
+            Spells.Add(locateObject.Name, locateObject);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Spell longstrider = new Spell
+            {
+                Source = sourceTitle,
+                Name = "Longstrider",
+                SpellLv = SpellLv.first,
+                School = SchoolOfMagic.transmutation,
+                CastingTime = DndTime.action,
+                Range = Range.touch,
+                Verbal = true,
+                Somatic = true,
+                Materials = "a pinch or dirt",
+                Duration = StringTime.hour,
+                Description = "You touch a creature. The target's speed increases by 10 feet until the spell ends. At Higher Levels. When you cast this spell using a spell slot of 2nd level or higher, you can target one additional creature for each slot level above 1st.",
+                Classes = new string[] { },
+                SingleTarget = true
+            };
+            Spells.Add(longstrider.Name, longstrider);
 
             #endregion
         }
